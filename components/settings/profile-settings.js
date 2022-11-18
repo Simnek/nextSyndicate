a22be04c599b0f1ef712eaaa9f8ec7158c77def4
Simnek <alexmitrovic993@gmail.com>
2022-11-18 10:40:55 +0100

    changeProfile

---
+++ components/settings/profile-settings.js
@@ -0,0 +1,11 @@
+import classes from './profile-settings.module.css';
+
+const ProfileSettings = (props) => {
+  return (
+    <section>
+
+    </section>
+  )
+};
+
+export default ProfileSettings;
